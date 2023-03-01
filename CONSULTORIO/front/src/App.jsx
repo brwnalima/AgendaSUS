e81a906f8f41312f50f22e070e1d22a347530849
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2023-03-01 15:23:14 -0300

    1

--- CONSULTORIO/front/src/App.jsx
+++ CONSULTORIO/front/src/App.jsx
@@ -1,17 +1,26 @@
-import Menu from './components/navbar/Navbar'
-import Home from './components/pages/Home'
-import './App.css'
-import Sobre from './components/pages/Sobre'
+import { useState } from 'react';
+import UsersApi from './api/UsersApi';
+import CreateUserModal from './components/CreateUserModal/CreateUserModal';
+
+const usersApi = UsersApi();
 
 function App() {
+  const [showCreateUserModal, setShowCreateUserModal] = useState(false);
+
+  const handleCloseCreateUserModal = () => setShowCreateUserModal(false);
+
+  const handleShowCreateUserModal = () => setShowCreateUserModal(true);
 
   return (
-    <div className="App">
-      <Menu />
-      <Home />
-      <Sobre />
+    <div>
+      <button onClick={handleShowCreateUserModal}>Criar usuário</button>
+      <CreateUserModal
+        isModalOpen={showCreateUserModal}
+        handleClose={handleCloseCreateUserModal}
+        createUser={usersApi.createUser}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- CONSULTORIO/front/src/components/CreateUserModal/CreateUserModal.jsx
+++ CONSULTORIO/front/src/components/CreateUserModal/CreateUserModal.jsx
@@ -1,49 +1,54 @@
-import { Modal, Button, Form } from 'react-bootstrap'
+import { Modal, Button, Form } from 'react-bootstrap';
+import { useState } from 'react';
+
 function CreateUserModal(props) {
-  return(
-    <div
-      className="modal show"
-      style={{ display: 'block', position: 'initial' }}
-    >
+  const [formData, setFormData] = useState({
+    nome: '',
+    email: '',
+    senha: ''
+  });
+
+  const handleInputChange = (event) => {
+    const { id, value } = event.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { nome, email, senha } = formData;
+    props.createUser(nome, email, senha);
+    setFormData({ nome: '', email: '', senha: '' });
+  };
+
+  return (
+    <div className="modal show" style={{ display: 'block', position: 'initial' }}>
       <Modal show={props.isModalOpen}>
-        <Form onSubmit={(event) => {
-          props.createUser(event)
-        }}>
-        <Modal.Header closeButton onClick={props.handleClose}>
-          <Modal.Title>Criar Usuario</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <Form.Group controlId="nome">
-            <Form.Label>
-              Nome
-            </Form.Label>
-            <Form.Control type="text" />
-          </Form.Group>
-
-          <Form.Group controlId="email">
-            <Form.Label>
-              Email
-            </Form.Label>
-            <Form.Control type="text" />
-          </Form.Group>
-
-          <Form.Group controlId="senha">
-            <Form.Label>
-              Senha
-            </Form.Label>
-            <Form.Control type="password" />
-          </Form.Group>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button variant="secondary" onClick={props.handleClose}>Close</Button>
-          <Button variant="primary" type="submit">Salvar</Button>
-        </Modal.Footer>
+        <Form onSubmit={handleSubmit}>
+          <Modal.Header closeButton onClick={props.handleClose}>
+            <Modal.Title>Criar Usuario</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Group controlId="nome">
+              <Form.Label>Nome</Form.Label>
+              <Form.Control type="text" value={formData.nome} onChange={handleInputChange} />
+            </Form.Group>
+            <Form.Group controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="text" value={formData.email} onChange={handleInputChange} />
+            </Form.Group>
+            <Form.Group controlId="senha">
+              <Form.Label>Senha</Form.Label>
+              <Form.Control type="password" value={formData.senha} onChange={handleInputChange} />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={props.handleClose}>Close</Button>
+            <Button variant="primary" type="submit">Salvar</Button>
+          </Modal.Footer>
         </Form>
-      </Modal >
+      </Modal>
     </div>
-  )
+  );
 }
 
-export default CreateUserModal
+export default CreateUserModal;
