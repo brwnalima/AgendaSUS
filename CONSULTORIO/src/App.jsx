aed8a068b8d4e7ca4a323c8031d5724e5f7c2f55
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2023-03-01 19:45:59 -0300

    ok

--- CONSULTORIO/src/App.jsx
+++ CONSULTORIO/src/App.jsx
@@ -2,14 +2,15 @@ import Menu from './components/navbar/Navbar'
 import Home from './components/pages/Home'
 import './App.css'
 import Sobre from './components/pages/Sobre'
+import Login from './components/login/login'
 
 function App() {
 
   return (
     <div className="App">
-      <Menu />
-      <Home />
-      <Sobre />
+      <Menu/>
+      <Login/>
+      
     </div>
   )
 }
